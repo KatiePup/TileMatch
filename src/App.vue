4ab09e7aab540c11e026d882af8217c8bb9b2216
KatiePup <katie.c.a.baker@gmail.com>
2022-02-26 19:36:01 +0000

    Add src directory and App component

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script>
+import TileMatch from './components/TileMatch.vue'
+
+export default {
+  components: [
+    TileMatch
+  ]
+}
+</script>
+
+<template>
+  <TileMatch/>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
