1e1efb228e41596622e925d359568759ffe327f4
KatiePup <katie.c.a.baker@gmail.com>
2022-02-26 19:34:57 +0000

    Add config files

---
+++ vite.config.js
@@ -0,0 +1,23 @@
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import eslintPlugin from "vite-plugin-eslint";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue(), eslintPlugin()],
+  // resolve: {
+  //   alias: {
+  //     'node:stream': 'stream-browserify',
+  //   },
+  // },
+  optimizeDeps: {
+    exclude: [
+      // "url",
+      // "http",
+      // "stream",
+      // "ipfs",
+
+      "ipfs-http-client"
+    ],
+  },
+});
