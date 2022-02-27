de99b59cae70452462af60fa0c7e88c4482c257a
Jack Fletcher <jackowenfletcher@gmail.com>
2022-02-27 18:06:17 +0000

    Update eslint conf

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,10 +14,4 @@ module.exports = {
     'no-unused-vars': 'warn',
     'spaced-comment': 'warn',
   },
-  parser: 'vue-eslint-parser',
-  // parserOptions: {
-  //   parser: '@typescript-eslint/parser',
-  //   ecmaVersion: 2020,
-  //   sourceType: 'module'
-  // }
 }
